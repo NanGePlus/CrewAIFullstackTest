4713f9e77def67b76ddb4d8a89d8882777d44a3a
NanGePlus <justicenang@gmail.com>
2024-09-29 15:11:08 +0800

    Add files via upload

---
+++ crewaiBackend/crew.py
@@ -0,0 +1,131 @@
+# 核心功能:实现了一个基于CrewAI的多Agent协作，主要功能包括:
+# (1)Crew项目初始化:定义了CrewtestprojectCrew类，使用@CrewBase装饰器将其标记为CrewAI项目。此类管理代理和任务的配置
+# (2)模型初始化：在构造函数中，创建一个使用ChatOpenAI模型的实例，以便后续的任务执行
+# (3)任务和代理定义：
+# 使用@agent装饰器定义多个代理（如市场分析师、营销策略师和内容创作者），每个代理都配置了特定的工具和模型
+# 使用@task装饰器定义多个任务，包括研究任务、项目理解任务、营销策略任务、活动创意任务和文案创作任务，并为每个任务设置回调函数
+# Crew调度：通过@crew装饰器创建一个Crew实例，将定义的代理和任务组合，并指定其执行顺序
+# (4)启动Crew：定义kickoff方法，该方法接受输入，启动Crew执行，并处理任务的开始、完成和错误事件的记录
+# (5)事件记录：在任务执行过程中，通过append_event_callback和其他地方记录任务状态和输出，便于跟踪作业进度和结果
+
+
+# 导入第三方库
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool
+# 导入本应用程序提供的方法
+from utils.models import MarketStrategy, CampaignIdea, Copy
+from utils.jobManager import append_event
+
+
+
+# 定义了一个CrewtestprojectCrew类并应用了@CrewBase装饰器初始化项目
+# 这个类代表一个完整的CrewAI项目
+@CrewBase
+class CrewtestprojectCrew():
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
+	# 构造初始化函数，接受job_id作为参数，用于标识作业
+	def __init__(self, job_id, llm):
+		self.job_id = job_id
+		self.llm = llm
+
+	# 定义task的回调函数，在任务完成后记录输出事件
+	def append_event_callback(self,task_output):
+		print("Callback called: %s", task_output)
+		append_event(self.job_id, task_output.expected_output)
+
+	# 通过@agent装饰器定义一个函数，返回一个Agent实例
+	@agent
+	def lead_market_analyst(self) -> Agent:
+		return Agent(
+			config=self.agents_config['lead_market_analyst'],
+			verbose=True,
+			llm=self.llm,
+			tools=[SerperDevTool(), ScrapeWebsiteTool()],
+		)
+	@agent
+	def chief_marketing_strategist(self) -> Agent:
+		return Agent(
+			config=self.agents_config['chief_marketing_strategist'],
+			verbose=True,
+			llm=self.llm,
+			tools=[SerperDevTool(), ScrapeWebsiteTool()],
+		)
+	@agent
+	def creative_content_creator(self) -> Agent:
+		return Agent(
+			config=self.agents_config['creative_content_creator'],
+			verbose=True,
+			llm=self.llm
+		)
+
+	# 通过@task装饰器定义一个函数，返回一个Task实例
+	@task
+	def research_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['research_task'],
+			callback=self.append_event_callback,
+		)
+	@task
+	def project_understanding_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['project_understanding_task'],
+			callback=self.append_event_callback,
+
+		)
+	@task
+	def marketing_strategy_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['marketing_strategy_task'],
+			callback=self.append_event_callback,
+			output_json=MarketStrategy
+		)
+	@task
+	def campaign_idea_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['campaign_idea_task'],
+			callback=self.append_event_callback,
+			output_json=CampaignIdea
+
+		)
+	@task
+	def copy_creation_task(self) -> Task:
+		return Task(
+			config=self.tasks_config['copy_creation_task'],
+			callback=self.append_event_callback,
+			context=[self.marketing_strategy_task(), self.campaign_idea_task()],
+			output_json=Copy
+		)
+
+	# Crew类将agent和task组合成一个执行队列，并根据指定的执行流程进行任务调度
+	# 通过@crew装饰器定义crew，创建一个Crew实例
+	# agents=self.agents和tasks=self.tasks分别自动获取@agent和@task装饰器生成的agent和task
+	# process=Process.sequential指定agent执行顺序为顺序执行模式
+	# process=Process.hierarchical指定agent执行顺序为层次化执行
+	@crew
+	def crew(self) -> Crew:
+		return Crew(
+			agents=self.agents,
+			tasks=self.tasks,
+			process=Process.sequential,
+			verbose=True
+		)
+
+	# 定义启动Crew的函数，接受输入参数inputs
+	def kickoff(self,inputs):
+		if not self.crew():
+			append_event(self.job_id, "Crew not set up")
+			return "Crew not set up"
+
+		append_event(self.job_id, "Task Started")
+		try:
+			results = self.crew().kickoff(inputs=inputs)
+			print("crew中最终生成结果results：", results)
+			append_event(self.job_id, "Task Complete")
+
+			return results
+		except Exception as e:
+			append_event(self.job_id, f"An error occurred: {e}")
+			return str(e)
+
---
+++ crewaiBackend/main.py
@@ -0,0 +1,117 @@
+# 核心功能:实现了一个基于Flask的API后端服务，主要功能包括:
+# (1)创建Flask应用：实例化Flask应用并启用CORS，以支持跨域请求
+# (2)环境变量设置：设置SERPER_API_KEY环境变量，用于Google搜索引擎API的访问
+# (3)作业管理：通过jobs字典管理并存储作业的状态和事件，确保在多线程环境中安全访问
+# (4)启动Crew:
+# kickoff_crew函数接受作业ID和输入参数，创建CrewtestprojectCrew实例并调用其kickoff方法
+# 在执行过程中捕获异常并更新作业状态
+# 使用线程异步执行作业，允许同时处理多个请求
+# (5)POST接口 /api/crew：接收客户请求，验证输入数据，生成作业ID，启动kickoff_crew函数，并返回作业ID和HTTP状态码202
+# (6)GET接口 /api/crew/<job_id>：根据作业ID查询作业状态，返回作业的状态、结果和相关事件
+
+
+# 导入python标准库
+from datetime import datetime
+import json
+from threading import Thread
+from uuid import uuid4
+import os
+# 导入第三方库
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
+# 导入本应用程序提供的方法
+from crew import CrewtestprojectCrew
+from utils.jobManager import append_event, jobs, jobs_lock, Event
+from utils.myLLM import my_llm
+
+
+# 服务访问的端口
+PORT = 8012
+# 设置SERPER_API_KEY环境变量，用于Google搜索引擎的API
+os.environ["SERPER_API_KEY"] = "9d2aa95f5a0831110a3ec837996ff37b906e99dd"
+# 选择大模型类型 openai:调用gpt大模型;oneapi:调用非gpt大模型;ollama:调用本地大模型
+LLM_TYPE = "openai"
+
+
+# 创建Flask应用实例
+app = Flask(__name__)
+# 启用CORS，处理跨域资源共享以便任何来源的请求可以访问以/api/开头的接口
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
+# 定义函数kickoff_crew实现运行crew
+# 接受job_id和inputs作为参数
+def kickoff_crew(job_id, inputs):
+    global LLM_TYPE
+    print(f"Crew for job {job_id} is starting")
+    # 定义变量 存放结果
+    results = None
+    try:
+        # 创建CrewtestprojectCrew实例并调用其kickoff方法，处理输入并获取结果
+        # openai:调用gpt大模型;oneapi:调用非gpt大模型;ollama:调用本地大模型
+        results = CrewtestprojectCrew(job_id, my_llm(LLM_TYPE)).kickoff(inputs)
+        print(f"Crew for job {job_id} is complete", results)
+
+    except Exception as e:
+        # 捕获异常，记录错误并更新作业状态为ERROR
+        print(f"Error in kickoff_crew for job {job_id}: {e}")
+        append_event(job_id, f"An error occurred: {e}")
+        with jobs_lock:
+            jobs[job_id].status = 'ERROR'
+            jobs[job_id].result = str(e)
+    # 在任务锁下，将任务状态更新为COMPLETE，存储结果并记录完成事件
+    with jobs_lock:
+        jobs[job_id].status = 'COMPLETE'
+        jobs[job_id].result = results
+        jobs[job_id].events.append(
+            Event(timestamp=datetime.now(), data="Crew complete"))
+
+
+# 定义POST接口/api/crew，运行crew
+@app.route('/api/crew', methods=['POST'])
+def run_crew():
+    print(f"Received request to run crew")
+    # 从请求中提取JSON数据，进行验证
+    data = request.json
+    if not data or 'customer_domain' not in data or 'project_description' not in data:
+        abort(400, description="Invalid input data provided.")
+    print(f"接收到的信息customer_domain: {data['customer_domain']},接收到的信息project_description: {data['project_description']}")
+    # 构建参数调用kickoff_crew函数运行crew
+    inputs = {
+        "customer_domain": data['customer_domain'],
+        "project_description": data['project_description']
+    }
+    job_id = str(uuid4())
+    # 启动一个新线程来执行kickoff_crew函数
+    thread = Thread(target=kickoff_crew, args=(
+        job_id, inputs))
+    thread.start()
+    # 返回作业ID和HTTP状态码202（接受）
+    return jsonify({"job_id": job_id}), 202
+
+
+# 定义GET接口/api/crew/<job_id>，获取特定作业的状态
+@app.route('/api/crew/<job_id>', methods=['GET'])
+def get_status(job_id):
+    # 检查作业是否存在jobs字典中
+    with jobs_lock:
+        job = jobs.get(job_id)
+        if job is None:
+            abort(404, description="Job not found")
+     # 尝试解析作业结果为JSON格式。如果解析失败，保持原始字符串
+    try:
+        result_json = json.loads(str(job.result))
+    except json.JSONDecodeError:
+        result_json = str(job.result)
+    # 返回作业ID、状态、结果和事件的JSON响应
+    return jsonify({
+        "job_id": job_id,
+        "status": job.status,
+        "result": result_json,
+        "events": [{"timestamp": event.timestamp.isoformat(), "data": event.data} for event in job.events]
+    })
+
+
+if __name__ == '__main__':
+    print(f"在端口 {PORT} 上启动服务器")
+    app.run(debug=True, port=PORT)
